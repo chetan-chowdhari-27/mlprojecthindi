24bc009ccb0347c77a3cc3b4f9e5e92579c5a205
chetan <chetanchaudhari90014@gmail.com>
2023-09-08 22:57:56 +0530

    data ingestion changes

---
+++ app.py
@@ -0,0 +1,19 @@
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+import sys
+import os
+
+
+if __name__ == "__main__":
+    logging.info("the execution has started")  # Use lowercase "info" instead of "INFO"
+
+    try:
+        # data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+    except Exception as e:
+        logging.info("Custom Exception")  # Use lowercase "info" instead of "INFO"
+        raise CustomException(e, sys)
