cb847dc078de8fea99575aff6185687b2afa6181
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-08-08 22:52:42 +0530

    template_files created

---
+++ template.py
@@ -0,0 +1,45 @@
+import os    # To check and provide generic path 
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "ML_Project"
+# project_dir = "C:\Users\win10\Desktop\Projects\" + project_name
+# C:\Users\win10\Desktop\Projects\ML_Project\template.py 
+list_of_files = [
+    # ".github/{project_name}/.gitkeep",
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/components/data_ingestion.py',
+    f'src/{project_name}/components/data_transformation.py',
+    f'src/{project_name}/components/model_training.py',
+    f'src/{project_name}/components/model_monitoring.py',
+    f'src/{project_name}/pipelines/__init__.py',
+    f'src/{project_name}/pipelines/training_pipeline.py',
+    f'src/{project_name}/pipelines/prediction_pipeline.py',
+    f'src/{project_name}/exception.py',
+    f'src/{project_name}/logger.py',
+    f'src/{project_name}/utlis.py',
+    'app.py',
+    'Dockerfile',
+    'requirements.txt',
+    'setup.py'
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filepath != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating directory: {filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass 
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
